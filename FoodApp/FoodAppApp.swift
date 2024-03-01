2532641f313e497d6a4a53787bf49c23aae6e150
nganbuiwx <ngan.bui.wx@renesas.com>
2024-03-01 13:44:15 +0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by rvc on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
